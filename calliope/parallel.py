07458299396e0445ebfadc402ce0a40f845bff2e
Stefan Pfenninger <stefan@pfenninger.org>
2013-12-01 15:03:51 +0000

    Improved parallel runner

--- calliope/parallel.py
+++ calliope/parallel.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import division
 
 import argparse
+import copy
 import itertools
 import os
 import shutil
@@ -10,30 +11,32 @@ import sys
 import pandas as pd
 import yaml
 
+from .core import Model
 from . import utils
 
 
 class Parallelizer(object):
     """
     Args:
-        additional_lines : can override additional_lines setting from
-                           the parallel_settings.yaml file.
+        target_dir : path to output directory for parallel runs.
+        config_run : path to YAML file with run settings. If not given,
+                     ``{{ module }}/config/run.yaml`` is used as the
+                     default.
 
     """
-    def __init__(self, config=None, target_dir=None, additional_lines=None):
+    def __init__(self, target_dir, config_run=None):
         super(Parallelizer, self).__init__()
-        self.config_file = config
-        self.config = utils.AttrDict(yaml.load(open(config, 'r')))
+        config_path = os.path.join(os.path.dirname(__file__), 'config')
+        if not config_run:
+            config_run = os.path.join(config_path, 'run.yaml')
+        self.config_file = config_run
+        self.config = utils.AttrDict.from_yaml(config_run)
         self.target_dir = target_dir
-        if additional_lines:
-            self.config.additional_lines = additional_lines
 
     def generate_iterations(self):
-        keys = (['override.' + c for c in self.config.iterations_model.keys()]
-                + self.config.iterations_run.keys())
-        values = (self.config.iterations_model.values()
-                  + self.config.iterations_run.values())
-        df = pd.DataFrame(list(itertools.product(*values)), columns=keys)
+        iters = self.config.parallel.iterations
+        df = pd.DataFrame(list(itertools.product(*iters.values())),
+                          columns=iters.keys())
         return df
 
     def _write_modelcommands(self, f, settings):
@@ -46,43 +49,44 @@ class Parallelizer(object):
         c = self.config
         # Create output directory
         if self.target_dir:
-            out_dir = os.path.join(self.target_dir, c.name)
+            out_dir = os.path.join(self.target_dir, c.parallel.name)
         else:
-            out_dir = c.name
+            out_dir = c.parallel.name
         os.makedirs(out_dir)
         os.makedirs(os.path.join(out_dir, 'Runs'))
         os.makedirs(os.path.join(out_dir, 'Logs'))
         iterations = self.generate_iterations()
-        # Save run settings in out_dir to figure out what all the numbers mean!
+        # Save relevant settings in out_dir,
+        # to figure out what all the numbers mean!
         os.makedirs(os.path.join(out_dir, 'Output'))
         iterations.to_csv(os.path.join(out_dir, 'Output', 'iterations.csv'))
-        shutil.copy(self.config_file, os.path.join(out_dir, 'Output',
-                                                   'parallel_settings.yaml'))
+        parallel_f = os.path.join(out_dir, 'Output', 'parallel_settings.yaml')
+        c.parallel.to_yaml(parallel_f)
         #
         # SET UP ADDITIONAL FILES FOR ARRAY RUNS
         #
-        if c.style == 'array':
+        if c.parallel.style == 'array':
             array_submission = 'run.sh'
             array_run = 'array_run.sh'
             # Write array submission script
             with open(os.path.join(out_dir, array_submission), 'w') as f:
                 f.write('#!/bin/sh\n')
-                if c.environment == 'qsub':
+                if c.parallel.environment == 'qsub':
                     f.write('#$ -t 1-{}\n'.format(len(iterations)))
-                    f.write('#$ -N {}\n'.format(c.name))
+                    f.write('#$ -N {}\n'.format(c.parallel.name))
                     f.write('#$ -j y -o Logs/array_$JOB_ID.log\n')
                     f.write('#$ -cwd\n')
                     f.write('\n./{} '.format(array_run) + '${SGE_TASK_ID}\n\n')
-                elif c.environment == 'bsub':
-                    f.write('#BSUB -J {}[1-{}]\n'.format(c.name,
+                elif c.parallel.environment == 'bsub':
+                    f.write('#BSUB -J {}[1-{}]\n'.format(c.parallel.name,
                                                          len(iterations)))
-                    t = 'run_settings.resources.memory'
+                    t = 'parallel.resources.memory'
                     if c.get_key(t, default=False):
                         f.write('#BSUB -R "rusage[mem={}]"\n'.format(c.get_key(t)))
-                    t = 'run_settings.resources.threads'
+                    t = 'parallel.resources.threads'
                     if c.get_key(t, default=False):
                         f.write('#BSUB -n {}\n'.format(c.get_key(t)))
-                    t = 'run_settings.resources.wall_time'
+                    t = 'parallel.resources.wall_time'
                     if c.get_key(t, default=False):
                         f.write('#BSUB -W {}\n'.format(c.get_key(t)))
                     f.write('#BSUB -o Logs/lsf_%I.log\n')
@@ -94,70 +98,70 @@ class Parallelizer(object):
         #
         # WRITE SETTINGS AND SCRIPTS FOR EACH ITERATION
         #
+        # Always make sure we are saving outputs from iterations!
+        c.set_key('output.save', True)
         for row in iterations.iterrows():
+            iter_c = copy.copy(c)
             # Generate configuration object
             index, item = row
             index_str = '{:0>4d}'.format(index)
-            iteration_config = c.run_settings
             for k, v in item.to_dict().iteritems():
-                iteration_config.set_key(k, v)
-            # Always make sure we are saving outputs from iterations!
-            iteration_config.set_key('output.save', True)
+                iter_c.set_key(k, copy.copy(v))
             # Set output dir in configuration object (NB: relative path!)
-            iteration_config.set_key('output.path', os.path.join('Output',
-                                                                 index_str))
+            iter_c.set_key('output.path', os.path.join('Output', index_str))
             # Write configuration object to YAML file
             settings = 'settings_{}.yaml'.format(index_str)
-            with open(os.path.join(out_dir, 'Runs', settings), 'w') as f:
-                yaml.dump(iteration_config.as_dict(), f)
-            if c.style == 'single':
+            iter_c.to_yaml(os.path.join(out_dir, 'Runs', settings))
+            if c.parallel.style == 'single':
                 # Write model run script
                 run = 'run_{}.sh'.format(index_str)
                 with open(os.path.join(out_dir, run), 'w') as f:
                     f.write('#!/bin/sh\n')
-                    if c.additional_lines:
-                        f.write(c.additional_lines + '\n')
+                    if c.parallel.additional_lines:
+                        f.write(c.parallel.additional_lines + '\n')
                     # Set job name
-                    if c.environment == 'bsub':
-                        if c.get_key('run_settings.limit_memory',
-                                     default=False):
-                            mem = c.get_key('run_settings.limit_memory')
-                            f.write('#BSUB -R "rusage[mem={}]"\n'.format(mem))
-                        f.write('#BSUB -J {}{}\n'.format(c.name, index_str))
+                    if c.parallel.environment == 'bsub':
+                        f.write('#BSUB -J {}{}\n'.format(c.parallel.name,
+                                                         index_str))
                     # Write model commands
                     self._write_modelcommands(f, settings)
                 os.chmod(os.path.join(out_dir, run), 0755)
-            elif c.style == 'array':
+            elif c.parallel.style == 'array':
                 with open(os.path.join(out_dir, array_run), 'a') as f:
                     # Write index + 1 because the array jobs are 1-indexed
                     f.write('{}) '.format(index + 1))
-                    if c.additional_lines:
-                        f.write(c.additional_lines + '\n')
+                    if c.parallel.additional_lines:
+                        f.write(c.parallel.additional_lines + '\n')
                     self._write_modelcommands(f, settings)
                     f.write(';;\n\n')
         # Final tasks after going through all iterations
-        if c.style == 'array':
+        if c.parallel.style == 'array':
             with open(os.path.join(out_dir, array_run), 'a') as f:
                 f.write('esac\n')
-        os.chmod(os.path.join(out_dir, array_submission), 0755)
-        os.chmod(os.path.join(out_dir, array_run), 0755)
+            os.chmod(os.path.join(out_dir, array_submission), 0755)
+            os.chmod(os.path.join(out_dir, array_run), 0755)
 
 
 def main():
     arguments = sys.argv[1:]
     parser = argparse.ArgumentParser(description='Run the Calliope model.')
     parser.add_argument('settings', metavar='settings', type=str, default='',
-                        help='parallel_settings file to use')
+                        help='Run settings file to use.')
     parser.add_argument('-s', '--single', dest='single', action='store_const',
                         const=True, default=False,
-                        help='don\'t do a parallel run, '
-                             'interpret settings file as '
-                             'run_settings instead of parallel_settings')
+                        help='Ignore `parallal` section in run settings, '
+                             'and only do a single run.')
     parser.add_argument('-d', '--dir', type=str, default='runs',
-                        help='target directory (default: runs)')
+                        help='Target directory for parallel runs '
+                             '(default: `runs`).')
     args = parser.parse_args(arguments)
-    parallelizer = Parallelizer(args.settings, target_dir='runs')
-    parallelizer.generate_runs()
+    if args.single:
+        model = Model(config_run=args.settings)
+        model.run()
+    else:
+        parallelizer = Parallelizer(target_dir=args.dir,
+                                    config_run=args.settings)
+        parallelizer.generate_runs()
 
 
 if __name__ == '__main__':
