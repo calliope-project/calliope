7620bf25ccfa756ca11d761e476a5678d9ccd747
Stefan Pfenninger <stefan@pfenninger.org>
2013-12-01 10:26:31 +0000

    Added several tests solving simple models

--- calliope/test/common.py
+++ calliope/test/common.py
@@ -2,6 +2,7 @@
 
 
 import cStringIO as StringIO
+import os
 
 import calliope
 
@@ -10,15 +11,23 @@ def assert_almost_equal(x, y, tolerance=0.0001):
     assert abs(x-y) < tolerance
 
 
-def simple_model(config_techs='test/common/techs_minimal.yaml',
-                 config_nodes='test/common/nodes_minimal.yaml',
-                 path='test/common/t_1h',
+def _add_test_path(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+def simple_model(config_techs=None, config_nodes=None, path=None,
                  config_run=None):
+    if not config_techs:
+        config_techs = _add_test_path('common/techs_minimal.yaml')
+    if not config_nodes:
+        config_nodes = _add_test_path('common/nodes_minimal.yaml')
+    if not path:
+        path = _add_test_path('common/t_1h')
     if not config_run:
         config_run = """
         input:
-            techs: {techs}
-            nodes: {nodes}
+            techs: '{techs}'
+            nodes: '{nodes}'
             path: '{path}'
         output:
             save: false
---
+++ calliope/test/test_model_onesite_subnodes.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+from __future__ import division
+
+import pytest
+import tempfile
+
+import common
+
+
+class TestModel:
+    @pytest.fixture(scope='module')
+    def model_one_tech_one_site(self):
+        nodes = """
+            nodes:
+                1:
+                    level: 1
+                    within:
+                    techs: ['demand']
+                    override:
+                        demand:
+                            constraints:
+                                r: -50
+                sub1,sub2:
+                    level: 0
+                    within: 1
+                    techs: ['ccgt']
+                    override:
+                        ccgt:
+                            constraints:
+                                e_cap_max: 25
+            links:
+        """
+        config_run = """
+            input:
+                techs: {techs}
+                nodes: {nodes}
+                path: '{path}'
+            output:
+                save: false
+            subset_t: ['2005-01-01', '2005-01-02']
+        """
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(nodes)
+            f.read()
+            model = common.simple_model(config_run=config_run,
+                                        config_nodes=f.name)
+        model.run()
+        return model
+
+    def test_one_tech_one_site_solves(self, model_one_tech_one_site):
+        model = model_one_tech_one_site
+        assert str(model.results.Solution.Status) == 'optimal'
+
+    def test_one_tech_one_site_balanced(self, model_one_tech_one_site):
+        model = model_one_tech_one_site
+        df = model.get_system_variables()
+        assert df['ccgt'].mean() == 50
+        assert (df['ccgt'] == -1 * df['demand']).all()
+
+    def test_one_tech_one_site_costs(self, model_one_tech_one_site):
+        model = model_one_tech_one_site
+        df = model.get_costs()
+        assert df.at['lcoe', 'total', 'ccgt'] == 0.1
