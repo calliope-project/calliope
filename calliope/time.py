Fixes to TimeSummarizer, re-added masking functionality

calliope/time.py
@@ -28,8 +28,13 @@ class TimeSummarizer(object):
 
     def reduce_resolution(self, data, resolution, t_range=None):
         """
-        Warning: modifies the passed data object in-place. Does not
-        return anything on success.
+        Reduces the resolution of the entire given ``data`` to ``resolution``,
+        which must be an integer and ``>1``.
+
+        The data must have a resolution of ``1`` to begin with.
+
+        Warning: modifies the passed data object in-place.
+        Returns None on success.
 
         """
         # Initialize some common data
@@ -52,59 +57,62 @@ class TimeSummarizer(object):
                 how = self.known_data_types[k]
                 if isinstance(data[k], utils.AttrDict):
                     for kk in data[k].keys():
-                        self.apply_method(data, how, s, key=k, subkey=kk)
+                        self._apply_method(data, how, s, key=k, subkey=kk)
                 else:
-                    self.apply_method(data, how, s, key=k)
+                    self._apply_method(data, how, s, key=k)
         # If not t_range (implies working on entire time series), also add
         # time_res to dataset (if t_range set, this happens inside
         # dynamic_timestepper)
         if not t_range:
             data['time_res_series'] = pd.Series(resolution, index=data['_t'])
 
-    def apply_method(self, data, how, s, key, subkey=None):
+    def _apply_method(self, data, how, s, key, subkey=None):
         if len(how) == 2:
             method = self.methods[how[0]]
             if subkey:
                 df = method(data[key][subkey][s], data[how[1]][subkey])
             else:
                 df = method(data[key][s], data[how[1]])
-            # If no slice, this implies we are working on whole time series,
-            # so we replace the existing time series completely
-            # to get around indexing problems
-            if s == slice(None):
-                if subkey:
-                    data[key][subkey] = df
-                else:
-                    data[key] = df
-            else:
-                if subkey:
-                    data[key][subkey][s] = df
-                else:
-                    data[key[s]] = df
         else:
             method = self.methods[how]
             if subkey:
                 df = method(data[key][subkey][s])
             else:
                 df = method(data[key][s])
-            if s == slice(None):
-                if subkey:
-                    data[key][subkey] = df
-                else:
-                    data[key] = df
+        # If no slice, this implies we are working on whole time series,
+        # so we replace the existing time series completely
+        # to get around indexing problems
+        if s == slice(None):
+            if subkey:
+                data[key][subkey] = df
             else:
-                if subkey:
-                    data[key][subkey][s] = df
-                else:
-                    data[key][s] = df
+                data[key] = df
+        else:
+            # Choose different iterator method for series and dataframe
+            if isinstance(df, pd.Series):
+                iterator = df.iteritems
+            else:
+                iterator = df.iterrows
+            # Now iterate over each item in the reduced-resolution df,
+            # updating the dataset item by item
+            if subkey:
+                for i, r in iterator():
+                    data[key][subkey].loc[i] = r
+            else:
+                for i, r in iterator():
+                    data[key].loc[i] = r
 
     def dynamic_timestepper(self, data, mask):
-        """`mask` must be a df with the same index as the other dfs in
-        data, and a 'summarize' column of 0s and 1s, such that contiguous
-        groups of 1s are compressed into a single time step
+        """``mask`` can be a simple or complex mask.
 
-        Warning: modifies the passed data object in-place. Does not
-        return anything on success.
+        If simple, it must be a df with the same index as the other dfs in
+        data, and a 'summarize' column containing only ``0`` and ``1``.
+        Contiguous groups of ``1`` are compressed into a single time step.
+
+        If complex, TODO
+
+        Warning: modifies the passed data object in-place.
+        Returns None on success.
 
         """
         # Set up the mask
@@ -130,17 +138,22 @@ class TimeSummarizer(object):
             istart = ito
         for k in data.keys():
             # Special case for `_t`, which is the only known_data_type which is always 0-indexed
-            # To get around non-matching index, we simply turn the boolean mask df into a list
-            if k == '_t':
+            if k == '_t' or k == '_dt':
+                # To get around non-matching index, we simply turn the boolean mask df into a list
                 data[k] = data[k][(df.summarize < 2).tolist()]
             elif k in self.known_data_types.keys():
-                data[k] = data[k][df.summarize < 2]
+                if isinstance(data[k], utils.AttrDict):
+                    for kk in data[k].keys():
+                        data[k][kk] = data[k][kk][df.summarize < 2]
+                else:
+                    data[k] = data[k][df.summarize < 2]
         df = df[df.summarize < 2]
         data['time_res_series'] = df['time_res']
 
     def _reduce_weighted_average(self, target, weight):
-        """Custom weighted average. NB: Currently non-functional and
-        not used.
+        """Custom weighted average using ``weight``.
+
+        NB: Currently non-functional and not used.
 
         """
         df = target.reindex(self.new_index)
@@ -152,39 +165,33 @@ class TimeSummarizer(object):
             weighted = weighted / weight.iloc[i*self.resolution:i*self.resolution+self.resolution, :].sum()
             weighted[weighted.isnull()] = 0
             df.iloc[i, :] = weighted
-        df = df.fillna(np.inf)
         target = df
         return target
 
-    def _reduce_average(self, df):
-        """Calculates rolling mean, then reindexes with ``self.new_index``.
+    def _infinity_test(self, df):
+        return (df.sum(axis=0) == np.inf).all()
+        # The above approach wouldn't work in a df with mixed inf and non-inf
+        # values, but this sho0.uldn't happy in practice anyway
+        # -- if a df contains inf it should be all inf!
 
-        """
-        df = pd.stats.moments.rolling_mean(df, self.resolution)
+    def _reduce_average(self, df):
+        """Calculates rolling mean, then reindexes with ``self.new_index``."""
+        if not self._infinity_test(df):
+            df = pd.stats.moments.rolling_mean(df, self.resolution)
         df = df.reindex(self.rolling_new_index)
         df.index = self.new_index
-        # Bad hack: if we have np.inf in input data, it gets replaced
-        # by np.nan in the rolling stats functions. The following addresses
-        # this, but would also replace np.nan caused by other reasons with
-        # np.inf, which may lead to strange behavior!
-        # (applies to _reduce_sum too)
-        df = df.fillna(np.inf)
         return df
 
     def _reduce_sum(self, df):
-        """Calculates rolling sum, then reindexes with ``self.new_index``.
-
-        """
-        df = pd.stats.moments.rolling_sum(df, self.resolution)
+        """Calculates rolling sum, then reindexes with ``self.new_index``."""
+        if not self._infinity_test(df):
+            df = pd.stats.moments.rolling_sum(df, self.resolution)
         df = df.reindex(self.rolling_new_index)
         df.index = self.new_index
-        df = df.fillna(np.inf)
         return df
 
     def _reduce_cut(self, df):
-        """Removes rows not in ``self.new_index``.
-
-        """
+        """Removes rows not in ``self.new_index``."""
         if len(self.new_index) == 1:
             df = pd.Series(df.iloc[0], index=self.new_index)
         else: