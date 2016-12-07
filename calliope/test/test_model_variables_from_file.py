ada65297d4656babf3caecd52ba8c62899c77ed1
Bryn Pickering <brynmorpickering@gmail.com>
2016-12-07 13:56:47 +0000

    Revert "minor import fixes"
    This reverts commit c90d7bcb6e5ba68148f4fb1d98c5ce63a57dc3f1.

--- calliope/test/test_model_variables_from_file.py
+++ calliope/test/test_model_variables_from_file.py
@@ -1,10 +1,12 @@
+import pytest
 import tempfile
 
 from calliope.utils import AttrDict
 from . import common
-from .common import solver, solver_io, _add_test_path
+from .common import assert_almost_equal, solver, solver_io
 
-def create_and_run_model(override, iterative_warmstart=False):
+def create_and_run_model(override, iterative_warmstart=False,
+                         demand_file='demand-static_r.csv'):
     locations = """
         locations:
             1:
@@ -14,62 +16,68 @@ def create_and_run_model(override, iterative_warmstart=False):
                         constraints:
                             e_cap.max: 100
                     demand_power:
+                        x_map: '1: demand'
                         constraints:
-                            r: -10
+                            r: file={demand_file}
         links:
     """
     config_run = """
         mode: operate
         model: ['{techs}', '{locations}']
-        subset_t: ['2005-01-01', '2005-01-02']
+         subset_t: ['2005-01-01', '2005-01-02']
     """
     override = AttrDict.from_yaml_string(override)
     override.set_key('solver', solver)
     override.set_key('solver_io', solver_io)
     with tempfile.NamedTemporaryFile(delete=False) as f:
-        f.write(locations.encode('utf-8'))
+        f.write(locations.format(demand_file=demand_file).encode('utf-8'))
         f.read()
         model = common.simple_model(config_run=config_run,
                                     config_locations=f.name,
                                     override=override,
-                                    path=_add_test_path('common/t_constraints_from_file'))
+                                    path=_add_test_path('common/t_time'))
     model.run(iterative_warmstart)
     return model
 
 class TestModel:
  
     # all constraints are fixed values
-    def test_model_fixed(self):
-        override = """override:"""
-        model = create_and_run_model(override)
+    def test_model_fixed(self, model):
+        override = None
+        assert str(model.results.solver.termination_condition) == 'optimal'
+    
+    # Demand is a timeseries variable
+    def test_model_var_demand(self, model):
+        override = None
+        model1 = create_and_run_model(demand_file=demand)
         assert str(model.results.solver.termination_condition) == 'optimal'
 
     # e_eff is a timeseries variable
-    def test_model_var_e_eff(self):
+    def test_model_var_e_eff(self, model):
         override = """
             override:
                 techs:
                     ccgt:
                         constraints:
-                            e_eff: file=eff_var_sin.csv
+                            e_eff: file=eff.csv
                     """
-        model = create_and_run_model(override)
+        model1 = create_and_run_model(override)
         assert str(model.results.solver.termination_condition) == 'optimal'
 
     # r_eff is a timeseries variable
-    def test_model_var_r_eff(self):
+    def test_model_var_r_eff(self, model):
         override = """
             override:
                 techs:
                     ccgt:
                         constraints:
-                            r_eff: file=eff_var_sin.csv
+                            r_eff: file=eff.csv
                     """
-        model = create_and_run_model(override)
+        model1 = create_and_run_model(override)
         assert str(model.results.solver.termination_condition) == 'optimal'
 
     # costs are a timeseries variables
-    def test_model_var_om_var(self):
+    def test_model_var_om_var(self, model):
         override = """
             override:
                 techs:
@@ -79,11 +87,11 @@ class TestModel:
                                 om_fuel: file=cost_rev_var.csv
                                 om_var: file=cost_rev_var.csv
                     """
-        model = create_and_run_model(override)
+        model1 = create_and_run_model(override)
         assert str(model.results.solver.termination_condition) == 'optimal'
 
     # costs are a timeseries variables
-    def test_model_var_sub_var(self):
+    def test_model_var_sub_var(self, model):
         override = """
             override:
                 techs:
@@ -92,5 +100,5 @@ class TestModel:
                             monetary:
                                 sub_var: file=cost_rev_var.csv
                     """
-        model = create_and_run_model(override)
+        model1 = create_and_run_model(override)
         assert str(model.results.solver.termination_condition) == 'optimal'
