30d2c37b894de8611de57cf5d88f2a34a152b485
Bryn Pickering <bp325@cam.ac.uk>
2017-01-10 16:50:59 +0000

    Add profiling to make.bat

--- make.bat
+++ make.bat
@@ -10,4 +10,19 @@ if "%1" == "lint" (
     goto end
 )
 
+rem Note that `--python` removed from `mprof run` compared to BASH file counterpart
+if "%1" == "profile" (
+    set PROFILE=profile_%date:~-4%-%date:~3,2%-%date:~0,2%-%time:~0,2%-%time:~3,2%-%time:~6,2%
+	mprof run -C -T 1.0 calliope run calliope/example_model/run_profiling.yaml --profile --profile_filename=%PROFILE%.profile
+	pyprof2calltree -i %PROFILE%.profile -o %PROFILE%.calltree
+	gprof2dot -f callgrind %PROFILE%.calltree | dot -Tsvg -o %PROFILE%.callgraph.svg
+    goto end
+)
+
+if "%1" == "profile-clean" (
+    del profile_*
+	del mprofile_*
+    goto end
+)
+
 :end
