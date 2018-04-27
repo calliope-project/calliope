97bd4622dcba0c9e84f184d7bca4e26411a6bfff
Bryn Pickering <bp325@cam.ac.uk>
2018-04-27 18:16:04 +0200

    Make Hound happier

--- calliope/test/test_core_preprocess.py
+++ calliope/test/test_core_preprocess.py
@@ -281,10 +281,14 @@ class TestChecks:
             excinfo, 'Unrecognised setting in run configuration: subset_time'
         )
 
-        override5 = {'run.objective': 'minmax_cost_optimization',
-                     'run.objective_options': {'cost_class': 'monetary',
-                                              'sense': 'minimize',
-                                              'unused_option': 'some_value'}}
+        override5 = {
+            'run.objective': 'minmax_cost_optimization',
+            'run.objective_options': {
+                'cost_class': 'monetary',
+                'sense': 'minimize',
+                'unused_option': 'some_value'
+            }
+        }
 
         with pytest.warns(exceptions.ModelWarning) as excinfo:
             build_model(override_dict=override5, override_groups='simple_supply')
