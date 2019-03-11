804d08270b8116ade23cdbf2ad53bf228ab12aa0
Tim Tröndle <tim.troendle@usys.ethz.ch>
2019-03-11 11:07:23 +0100

    Fix the constraint set over which group constraint is defined

--- calliope/backend/pyomo/constraints/group.py
+++ calliope/backend/pyomo/constraints/group.py
@@ -30,12 +30,12 @@ def load_constraints(backend_model):
         )
     if 'group_energy_cap_share_min' in model_data_dict:
         backend_model.group_energy_cap_share_min_constraint = po.Constraint(
-            backend_model.constraint_groups,
+            backend_model.group_names_energy_cap_share_min,
             ['min'], rule=energy_cap_share_constraint_rule
         )
     if 'group_energy_cap_share_max' in model_data_dict:
         backend_model.group_energy_cap_share_max_constraint = po.Constraint(
-            backend_model.constraint_groups,
+            backend_model.group_names_energy_cap_share_max,
             ['max'], rule=energy_cap_share_constraint_rule
         )
 
@@ -111,12 +111,6 @@ def energy_cap_share_constraint_rule(backend_model, constraint_group, what):
     """
     model_data_dict = backend_model.__calliope_model_data__['data']
     share = model_data_dict['group_energy_cap_share_{}'.format(what)][(constraint_group)]
-    # FIXME uncomment this once Bryn has merged his changes
-    # and import again: from calliope.backend.pyomo.util import get_param
-    # share = get_param(
-    #     backend_model,
-    #     'group_demand_share_{}'.format(what), (carrier, constraint_group)
-    # )
 
     if np.isnan(share):
         return po.Constraint.NoConstraint
