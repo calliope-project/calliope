244c3ed02c0e3dda631c11d47808836f68b10ba9
Stefan Pfenninger <stefan@pfenninger.org>
2019-05-30 16:21:03 +0200

    Fix documentation not building

--- doc/helpers/generate_tables.py
+++ doc/helpers/generate_tables.py
@@ -70,13 +70,13 @@ def process():
     y = yaml.YAML()
 
     for tech_group in defaults['tech_groups']:
-        defaults = {
+        this_group_defaults = {
             'essentials': defaults['tech_groups'][tech_group].get('essentials', {}),
             'constraints': defaults['tech_groups'][tech_group].get('constraints', {}),
             'costs': defaults['tech_groups'][tech_group].get('costs', {})
         }
         with open('./user/includes/basetech_{}.yaml'.format(tech_group), 'w') as f:
-            f.write(yaml.dump(defaults, Dumper=yaml.RoundTripDumper))
+            f.write(yaml.dump(this_group_defaults, Dumper=yaml.RoundTripDumper))
 
         required_allowed = {
             'required_constraints': y.seq(defaults['tech_groups'][tech_group].get('required_constraints', [])),
