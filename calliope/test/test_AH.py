e82ba3816b1fb535c009e9693fd9870b35d43b61
Adriaan P Hilbers <a.hilbers17@imperial.ac.uk>
2020-10-14 10:09:51 +0100

    create reliable test setting

--- calliope/test/test_AH.py
+++ calliope/test/test_AH.py
@@ -1,5 +1,7 @@
 import os
+import pandas as pd
 import pdb
+
 
 from calliope.test.common.util import (
     build_test_model,
@@ -17,8 +19,26 @@ _MODEL_URBAN = os.path.join(
 
 
 def _dev_test():
-    model = build_test_model(model_file="model_operate.yaml")
-    model.run()
+    horizon_window_list = [
+        (24, 24), (48, 24), (72, 24), (96, 24), (48, 48), (72, 48), (96, 48),
+        (72, 72), (96, 72), (96, 96),
+    ]
+
+    results = pd.DataFrame(columns=['horizon', 'window', 'result'])
+    for i, (horizon, window) in enumerate(horizon_window_list):
+        override_dict = {'run.operation.horizon': horizon,
+                         'run.operation.window': window}
+        try:
+            model = build_test_model(model_file="model_operate.yaml",
+                                     override_dict=override_dict)
+            model.run()
+            results.loc[i] = [horizon, window, 'OK']
+        except IndexError:
+            results.loc[i] = [horizon, window, 'IndexError']
+        except ValueError:
+            results.loc[i] = [horizon, window, 'ValueError']
+
+    print(results)
 
 
 if __name__ == '__main__':
