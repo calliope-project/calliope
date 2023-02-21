35484809b90586b4967ffe779987c30af8a7f6fd
brynpickering <bryn.pickering@usys.ethz.ch>
2023-02-21 17:23:18 +0100

    Add type hinting backwards compatibility

--- calliope/backend/equation_parser.py
+++ calliope/backend/equation_parser.py
@@ -24,7 +24,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ##
-
+from __future__ import annotations
 from typing import Callable, Any, Union, Optional, Iterator
 
 import pyparsing as pp
