e9af17e2d4387eeff49aa3bb4b116100d714bd09
brynpickering <bryn.pickering@usys.ethz.ch>
2023-04-06 15:49:03 +0100

    Add helper func as_latex tests

--- calliope/backend/helper_functions.py
+++ calliope/backend/helper_functions.py
@@ -1,4 +1,5 @@
 import re
+from typing import Union, Any
 
 import xarray as xr
 
@@ -18,25 +19,7 @@ def inheritance(model_data, as_latex: bool = False, **kwargs):
 
 
 def imask_sum(model_data, as_latex: bool = False, **kwargs):
-    def _instr(dim):
-        return rf"\text{{{dim.removesuffix('s')}}} \in \text{{{dim}}}"
-
-    def _as_latex(component, *, over):
-        if isinstance(over, str):
-            overstring = _instr(over)
-        else:
-            overstring = r"\substack{" + r" \\ ".join(_instr(i) for i in over) + "}"
-        return rf"\sum\limits_{{{overstring}}} ({component})"
-
     def _imask_sum(component, *, over):
-        """
-        Args:
-            to_sum (_type_): _description_
-            over (_type_): _description_
-
-        Returns:
-            _type_: _description_
-        """
         to_return = model_data.get(component, xr.DataArray(False))
         if to_return.any():
             to_return = expression_sum()(to_return, over=over) > 0
@@ -44,22 +27,12 @@ def imask_sum(model_data, as_latex: bool = False, **kwargs):
         return to_return
 
     if as_latex:
-        return _as_latex
+        return sum_as_latex
     else:
         return _imask_sum
 
 
 def expression_sum(as_latex: bool = False, **kwargs):
-    def _instr(dim):
-        return rf"\text{{{dim.removesuffix('s')}}} \in \text{{{dim}}}"
-
-    def _as_latex(component, *, over):
-        if isinstance(over, str):
-            overstring = _instr(over)
-        else:
-            overstring = r"\substack{" + r" \\ ".join(_instr(i) for i in over) + "}"
-        return rf"\sum\limits_{{{overstring}}} ({component})"
-
     def _expression_sum(component, *, over):
         """
 
@@ -86,9 +59,32 @@ def expression_sum(as_latex: bool = False, **kwargs):
         return to_return
 
     if as_latex:
-        return _as_latex
+        return sum_as_latex
     else:
         return _expression_sum
+
+
+def sum_as_latex(component: str, *, over: Union[str, list]) -> str:
+    """Shared utility function to generate the imask and expression latex summation string
+
+    Args:
+        component (str): Component name to sum.
+        over (Union[str, list]): either one dimension or list of dimension names over which to sum. Each is expected to end in "s".
+
+    Returns:
+        str: Valid LaTeX math summation string.
+    """
+
+    def _instr(dim):
+        dim_singular = dim.removesuffix("s")
+        return rf"\text{{{dim_singular}}} \in \text{{{dim}}}"
+
+    if isinstance(over, str):
+        overstring = _instr(over)
+    else:
+        foreach_string = r" \\ ".join(_instr(i) for i in over)
+        overstring = rf"\substack{{{foreach_string}}}"
+    return rf"\sum\limits_{{{overstring}}} ({component})"
 
 
 def squeeze_carriers(model_data, as_latex: bool = False, **kwargs):
@@ -111,9 +107,7 @@ def squeeze_carriers(model_data, as_latex: bool = False, **kwargs):
 
 def squeeze_primary_carriers(model_data, as_latex: bool = False, **kwargs):
     def _as_latex(component, carrier_tier):
-        return (
-            rf"\sum\limits_{{\text{{carrier=primary_carrier_{carrier_tier}}}}} ({component})"
-        )
+        return rf"\sum\limits_{{\text{{carrier=primary_carrier_{carrier_tier}}}}} ({component})"
 
     def _squeeze_primary_carriers(component, carrier_tier):
         return expression_sum(**kwargs)(
@@ -131,15 +125,9 @@ def squeeze_primary_carriers(model_data, as_latex: bool = False, **kwargs):
 
 def get_connected_link(model_data, as_latex: bool = False, **kwargs):
     def _as_latex(component):
-        try:
-            component_sections = component.split(r"_\text{")
-            sub_section = component_sections[1]
-        except IndexError:
-            return component
-        else:
-            sub_node = re.sub(r"\bnode\b", r"node=remote\_node", sub_section)
-            sub_node_tech = re.sub(r"\btech\b", r"tech=remote\_tech", sub_node)
-            return component_sections[0] + r"_\text{" + sub_node_tech
+        for iterator in ["node", "tech"]:
+            component = add_to_iterator(component, iterator, f"=remote_{iterator}")
+        return component
 
     def _get_connected_link(component):
         dims = [i for i in component.dims if i in ["techs", "nodes"]]
@@ -178,11 +166,7 @@ def roll(as_latex: bool = False, **kwargs):
     def _as_latex(component, **roll_kwargs):
         for k, v in roll_kwargs.items():
             k_singular = k.removesuffix("s")
-            component = re.sub(
-                rf"(_\\text{{)([\w,_]*?)(\b{k_singular}\b)([\w,_]*?)(}})",
-                rf"\1\2\3{-1 * int(v)}\4\5",
-                component,
-            )
+            component = add_to_iterator(component, k_singular, f"{-1 * int(v):+d}")
         return component
 
     def _roll(component, **roll_kwargs):
@@ -193,3 +177,31 @@ def roll(as_latex: bool = False, **kwargs):
         return _as_latex
     else:
         return _roll
+
+
+def add_to_iterator(instring: str, iterator: Any, new_string: Any) -> str:
+    """Find an iterator in the iterator substring of the component string
+    (anything wrapped in `_text{}`). Other parts of the iterator substring can be anything
+    except curly braces, e.g. the standalone `foo` will be found here and acted upon:
+    `\\textit{my_param}_\text{bar,foo,foo=bar,foo+1}`
+
+    Args:
+        instring (str): String in which the iterator substring can be found.
+        iterator (Any): The iterator to search for.
+        new_string (Any): The new string to **append** to the iterator name.
+
+    Returns:
+        str: `instring`, but with `iterator` replaced with `iterator + new_string`
+    """
+
+    def _replace_in_iterator(matched):
+        iterator_list = matched.group(2).split(",")
+        new_iterator_list = []
+        for it in iterator_list:
+            if it == iterator:
+                it += new_string
+            new_iterator_list.append(it)
+
+        return matched.group(1) + ",".join(new_iterator_list) + matched.group(3)
+
+    return re.sub(r"(_\\text{)([^{}]*?)(})", _replace_in_iterator, instring)
--- calliope/test/test_backend_helper_functions.py
+++ calliope/test/test_backend_helper_functions.py
@@ -6,92 +6,168 @@ import xarray as xr
 from calliope.backend import helper_functions
 
 
-def test_inheritance(dummy_model_data):
-    inheritance = helper_functions.inheritance(dummy_model_data)
-    boo_bool = inheritance("boo")
-    assert boo_bool.equals(dummy_model_data.boo_inheritance_bool)
+class TestFuncs:
+    def test_inheritance(self, dummy_model_data):
+        inheritance = helper_functions.inheritance(dummy_model_data)
+        boo_bool = inheritance("boo")
+        assert boo_bool.equals(dummy_model_data.boo_inheritance_bool)
 
+    def test_imask_sum_not_exists(self, dummy_model_data):
+        imask_sum = helper_functions.imask_sum(dummy_model_data)
+        summed = imask_sum("foo", over="techs")
+        assert summed.equals(xr.DataArray(False))
 
-def test_imask_sum_not_exists(dummy_model_data):
-    imask_sum = helper_functions.imask_sum(dummy_model_data)
-    summed = imask_sum("foo", over="techs")
-    assert summed.equals(xr.DataArray(False))
-
-
-@pytest.mark.parametrize(
-    ["var", "over", "expected"],
-    [("with_inf", "techs", "nodes_true"), ("all_nan", "techs", "nodes_false")],
-)
-def test_imask_sum_exists(dummy_model_data, var, over, expected):
-    imask_sum = helper_functions.imask_sum(dummy_model_data)
-    summed = imask_sum(var, over=over)
-    assert summed.equals(dummy_model_data[expected])
-
-
-@pytest.mark.parametrize("over", ["techs", ["techs"]])
-def test_expression_sum_one_dim(dummy_model_data, over):
-    expression_sum = helper_functions.expression_sum()
-    summed_array = expression_sum(dummy_model_data.only_techs, over=over)
-    assert not summed_array.shape
-    assert summed_array == 6
-
-
-@pytest.mark.parametrize("over", ["techs", ["techs"]])
-def test_expression_sum_one_of_two_dims(dummy_model_data, over):
-    expression_sum = helper_functions.expression_sum()
-    summed_array = expression_sum(dummy_model_data.with_inf, over=over)
-    assert summed_array.shape == (2,)
-    assert np.array_equal(summed_array, [5, np.inf])
-
-
-def test_expression_sum_two_dims(dummy_model_data):
-    expression_sum = helper_functions.expression_sum()
-    summed_array = expression_sum(
-        dummy_model_data.with_inf_as_bool, over=["nodes", "techs"]
+    @pytest.mark.parametrize(
+        ["var", "over", "expected"],
+        [("with_inf", "techs", "nodes_true"), ("all_nan", "techs", "nodes_false")],
     )
-    assert not summed_array.shape
-    assert summed_array == 5
+    def test_imask_sum_exists(self, dummy_model_data, var, over, expected):
+        imask_sum = helper_functions.imask_sum(dummy_model_data)
+        summed = imask_sum(var, over=over)
+        assert summed.equals(dummy_model_data[expected])
+
+    @pytest.mark.parametrize("over", ["techs", ["techs"]])
+    def test_expression_sum_one_dim(self, dummy_model_data, over):
+        expression_sum = helper_functions.expression_sum()
+        summed_array = expression_sum(dummy_model_data.only_techs, over=over)
+        assert not summed_array.shape
+        assert summed_array == 6
+
+    @pytest.mark.parametrize("over", ["techs", ["techs"]])
+    def test_expression_sum_one_of_two_dims(self, dummy_model_data, over):
+        expression_sum = helper_functions.expression_sum()
+        summed_array = expression_sum(dummy_model_data.with_inf, over=over)
+        assert summed_array.shape == (2,)
+        assert np.array_equal(summed_array, [5, np.inf])
+
+    def test_expression_sum_two_dims(self, dummy_model_data):
+        expression_sum = helper_functions.expression_sum()
+        summed_array = expression_sum(
+            dummy_model_data.with_inf_as_bool, over=["nodes", "techs"]
+        )
+        assert not summed_array.shape
+        assert summed_array == 5
+
+    def test_squeeze_carriers(self, dummy_model_data):
+        squeeze_carriers = helper_functions.squeeze_carriers(dummy_model_data)
+        squeezed = squeeze_carriers(dummy_model_data.all_true_carriers, "foo")
+
+        assert dummy_model_data.carrier.sel(carrier_tiers="foo").sum() == squeezed.sum()
+        assert not set(squeezed.dims).symmetric_difference(["techs"])
+
+    def test_squeeze_primary_carriers(self, dummy_model_data):
+        squeeze_carriers = helper_functions.squeeze_primary_carriers(dummy_model_data)
+        squeezed = squeeze_carriers(dummy_model_data.all_true_carriers, "out")
+
+        assert squeezed.sum() == 3
+        assert squeezed.max() == 1
+        assert not set(squeezed.dims).symmetric_difference(["techs"])
+
+    def test_squeeze_primary_carriers_not_in_model(self, dummy_model_data):
+        squeeze_carriers = helper_functions.squeeze_primary_carriers(dummy_model_data)
+        with pytest.raises(AttributeError):
+            squeeze_carriers(dummy_model_data.all_true_carriers, "foo")
+
+    def test_get_connected_link(self, dummy_model_data):
+        get_connected_link = helper_functions.get_connected_link(dummy_model_data)
+        connected_link = get_connected_link(dummy_model_data.with_inf)
+        assert np.array_equal(
+            connected_link, [[np.inf, np.nan, 2, 3], [3, 2, 1, np.nan]], equal_nan=True
+        )
+
+    @pytest.mark.parametrize(["ix", "expected"], [(0, "foo"), (1, "bar"), (-1, "bar")])
+    def test_get_val_at_index(self, dummy_model_data, ix, expected):
+        get_val_at_index = helper_functions.get_val_at_index(dummy_model_data)
+        assert get_val_at_index(dim="timesteps", idx=ix) == expected
+
+    @pytest.mark.parametrize(["to_roll", "expected"], [(1, 3), (1.0, 3), (-3.0, 3)])
+    def test_roll(self, dummy_model_data, to_roll, expected):
+        roll = helper_functions.roll()
+        rolled = roll(dummy_model_data.with_inf, techs=to_roll)
+        assert rolled.sel(nodes="foo", techs="foobar") == expected
 
 
-def test_squeeze_carriers(dummy_model_data):
-    squeeze_carriers = helper_functions.squeeze_carriers(dummy_model_data)
-    squeezed = squeeze_carriers(dummy_model_data.all_true_carriers, "foo")
+class TestAsLatex:
+    def test_inheritance(self, dummy_model_data):
+        inheritance = helper_functions.inheritance(dummy_model_data, as_latex=True)
+        assert inheritance("boo") == r"\text{tech_group=boo}"
 
-    assert dummy_model_data.carrier.sel(carrier_tiers="foo").sum() == squeezed.sum()
-    assert not set(squeezed.dims).symmetric_difference(["techs"])
+    def test_imask_sum_not_exists(self, dummy_model_data):
+        imask_sum = helper_functions.imask_sum(dummy_model_data, as_latex=True)
+        summed_string = imask_sum("foo", over="techs")
+        assert summed_string == r"\sum\limits_{\text{tech} \in \text{techs}} (foo)"
 
-
-def test_squeeze_primary_carriers(dummy_model_data):
-    squeeze_carriers = helper_functions.squeeze_primary_carriers(dummy_model_data)
-    squeezed = squeeze_carriers(dummy_model_data.all_true_carriers, "out")
-
-    assert squeezed.sum() == 3
-    assert squeezed.max() == 1
-    assert not set(squeezed.dims).symmetric_difference(["techs"])
-
-
-def test_squeeze_primary_carriers_not_in_model(dummy_model_data):
-    squeeze_carriers = helper_functions.squeeze_primary_carriers(dummy_model_data)
-    with pytest.raises(AttributeError):
-        squeeze_carriers(dummy_model_data.all_true_carriers, "foo")
-
-
-def test_get_connected_link(dummy_model_data):
-    get_connected_link = helper_functions.get_connected_link(dummy_model_data)
-    connected_link = get_connected_link(dummy_model_data.with_inf)
-    assert np.array_equal(
-        connected_link, [[np.inf, np.nan, 2, 3], [3, 2, 1, np.nan]], equal_nan=True
+    @pytest.mark.parametrize(
+        ["over", "expected_substring"],
+        [
+            ("techs", r"\text{tech} \in \text{techs}"),
+            (["techs"], r"\substack{\text{tech} \in \text{techs}}"),
+            (
+                ["nodes", "techs"],
+                r"\substack{\text{node} \in \text{nodes} \\ \text{tech} \in \text{techs}}",
+            ),
+        ],
     )
+    @pytest.mark.parametrize(
+        "func", [helper_functions.imask_sum, helper_functions.expression_sum]
+    )
+    def test_sum(self, dummy_model_data, over, expected_substring, func):
+        imask_sum = func(model_data=dummy_model_data, as_latex=True)
+        summed_string = imask_sum(r"\textit{with_inf}_\text{node,tech}", over=over)
+        assert (
+            summed_string
+            == rf"\sum\limits_{{{expected_substring}}} (\textit{{with_inf}}_\text{{node,tech}})"
+        )
 
+    def test_squeeze_carriers(self, dummy_model_data):
+        squeeze_carriers = helper_functions.squeeze_carriers(
+            dummy_model_data, as_latex=True
+        )
+        squeezed_string = squeeze_carriers("foo", "out")
+        assert (
+            squeezed_string
+            == r"\sum\limits_{\text{carrier} \in \text{carrier_tier(out)}} (foo)"
+        )
 
-@pytest.mark.parametrize(["ix", "expected"], [(0, "foo"), (1, "bar"), (-1, "bar")])
-def test_get_val_at_index(dummy_model_data, ix, expected):
-    get_val_at_index = helper_functions.get_val_at_index(dummy_model_data)
-    assert get_val_at_index(dim="timesteps", idx=ix) == expected
+    def test_squeeze_primary_carriers(self, dummy_model_data):
+        squeeze_carriers = helper_functions.squeeze_primary_carriers(
+            dummy_model_data, as_latex=True
+        )
+        squeezed_string = squeeze_carriers("foo", "out")
+        assert (
+            squeezed_string == r"\sum\limits_{\text{carrier=primary_carrier_out}} (foo)"
+        )
 
+    def test_get_connected_link(self, dummy_model_data):
+        get_connected_link = helper_functions.get_connected_link(
+            dummy_model_data, as_latex=True
+        )
+        connected_link_string = get_connected_link(r"\textit{node}_\text{node,tech}")
+        assert (
+            connected_link_string
+            == r"\textit{node}_\text{node=remote_node,tech=remote_tech}"
+        )
 
-@pytest.mark.parametrize(["to_roll", "expected"], [(1, 3), (1.0, 3), (-3.0, 3)])
-def test_roll(dummy_model_data, to_roll, expected):
-    roll = helper_functions.roll()
-    rolled = roll(dummy_model_data.with_inf, techs=to_roll)
-    assert rolled.sel(nodes="foo", techs="foobar") == expected
+    def test_get_val_at_index(self, dummy_model_data):
+        get_val_at_index = helper_functions.get_val_at_index(
+            dummy_model_data, as_latex=True
+        )
+        outstring = get_val_at_index(dim="timesteps", idx=1)
+        assert outstring == "timesteps[1]"
+
+    @pytest.mark.parametrize(
+        ["instring", "expected_substring"],
+        [
+            (r"\textit{foo}_\text{foo}", r"foo+1"),
+            (r"\textit{foo}_\text{bar}", r"bar"),
+            (r"\textit{foo}_\text{foo,bar}", r"foo+1,bar"),
+            (r"\textit{foo}_\text{foo}", r"foo+1"),
+            (r"\textit{foo}_\text{foobar,bar_foo,foo}", r"foobar,bar_foo,foo+1"),
+            (r"\textit{foo}_\text{foo=bar,foo}", r"foo=bar,foo+1"),
+            (r"\textit{foo}_\text{baz,foo,bar,foo}", r"baz,foo+1,bar,foo+1"),
+        ],
+    )
+    def test_roll(self, instring, expected_substring):
+        roll = helper_functions.roll(as_latex=True)
+        rolled_string = roll(instring, foo=-1)
+        assert rolled_string == rf"\textit{{foo}}_\text{{{expected_substring}}}"
