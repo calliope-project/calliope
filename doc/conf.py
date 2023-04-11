393540144e9cfb1166e6ab0527975bc9967586fb
brynpickering <bryn.pickering@usys.ethz.ch>
2023-04-11 15:46:31 +0100

    Add calliope back in to PATH in Sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,7 @@ from sphinx.builders.singlehtml import StandaloneHTMLBuilder, SingleFileHTMLBuil
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("."))
 sys.path.append(os.path.abspath("_themes"))
 
