4ef37af480494077a047a660f0abc94f1a1c5042
Francesco Sanvito <68587472+FraSanvit@users.noreply.github.com>
2023-11-10 11:10:50 +0100

    Allow timeseries parsing to group_constraints

--- calliope/preprocess/model_run.py
+++ calliope/preprocess/model_run.py
@@ -541,6 +541,7 @@ def process_timeseries_data(config_model, model_run, timeseries_dataframes):
     # Generate set of all files and dataframes we want to load
     location_config = model_run.locations.as_dict_flat()
     model_config = config_model.model.as_dict_flat()
+    group_constraint_config = config_model.group_constraints.as_dict_flat()
 
     # Find names of csv files (file=) or dataframes (df=) called in config
     def get_names(datatype, config):
@@ -554,8 +555,10 @@ def process_timeseries_data(config_model, model_run, timeseries_dataframes):
 
     constraint_filenames = get_names("file", location_config)
     cluster_filenames = get_names("file", model_config)
+    group_constraint_filenames = get_names("file", group_constraint_config)
     constraint_dfnames = get_names("df", location_config)
     cluster_dfnames = get_names("df", model_config)
+    group_constraint_dfnames = get_names("df", group_constraint_config)
 
     # Check if timeseries_dataframes is in the correct format (dict of
     # pandas DataFrames)
@@ -570,8 +573,10 @@ def process_timeseries_data(config_model, model_run, timeseries_dataframes):
         len(
             constraint_filenames
             | cluster_filenames
+            | group_constraint_filenames
             | constraint_dfnames
             | cluster_dfnames
+            | group_constraint_dfnames
         )
         == 0
     ):
@@ -583,12 +588,12 @@ def process_timeseries_data(config_model, model_run, timeseries_dataframes):
     # Load each timeseries into timeseries data. tskey is either a filename
     # (called by file=...) or a key in timeseries_dataframes (called by df=...)
     for tskey in (
-        constraint_filenames | cluster_filenames | constraint_dfnames | cluster_dfnames
+        constraint_filenames | cluster_filenames | group_constraint_filenames | constraint_dfnames | cluster_dfnames | group_constraint_dfnames
     ):  # Filenames or dict keys
         # If tskey is a CSV path, load the CSV, else load the dataframe
-        if tskey in constraint_filenames | cluster_filenames:
+        if tskey in constraint_filenames | cluster_filenames | group_constraint_filenames:
             df = load_timeseries_from_file(config_model, tskey)
-        elif tskey in constraint_dfnames | cluster_dfnames:
+        elif tskey in constraint_dfnames | cluster_dfnames | group_constraint_dfnames:
             df = load_timeseries_from_dataframe(timeseries_dataframes, tskey)
 
         try:
