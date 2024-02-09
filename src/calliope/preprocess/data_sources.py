1cdb69f91403700cc4e446ce046a8a9513764d4e
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2024-02-09 08:57:00 +0000

    Try setting a locale in the github action

--- src/calliope/preprocess/data_sources.py
+++ src/calliope/preprocess/data_sources.py
@@ -27,7 +27,7 @@ from calliope.util.tools import listify, relative_path
 
 LOGGER = logging.getLogger(__name__)
 
-locale.setlocale(locale.LC_ALL, "en_GB.utf-8")
+locale.setlocale(locale.LC_ALL, "")
 
 
 class DataSourceDict(TypedDict):
