f3c50b16f355bad0383c18c4fb3230bfa811ee0e
Ivan Ruiz Manuel <72193617+irm-codebase@users.noreply.github.com>
2024-07-02 15:26:46 +0200

    stringified include to avoid circular import

--- src/calliope/backend/__init__.py
+++ src/calliope/backend/__init__.py
@@ -1,5 +1,7 @@
 """Calliope's optimisation backend module."""
 
+from typing import TYPE_CHECKING
+
 import xarray as xr
 
 from calliope.backend.latex_backend_model import MathDocumentation
@@ -9,8 +11,11 @@ from calliope.exceptions import BackendError
 
 MODEL_BACKENDS = ("pyomo",)
 
+if TYPE_CHECKING:
+    from calliope.backend.backend_model import BackendModel
 
-def get_model_backend(name: str, data: xr.Dataset, **kwargs):
+
+def get_model_backend(name: str, data: xr.Dataset, **kwargs) -> "BackendModel":
     """Assign a backend using the given configuration.
 
     Args:
