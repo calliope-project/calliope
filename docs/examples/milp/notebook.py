5213c43089c04b4a1bd2bba931b8926566afa24d
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2026-07-24 19:49:39 +0100

    Remove kernelspec

--- docs/examples/milp/notebook.py
+++ docs/examples/milp/notebook.py
@@ -7,10 +7,6 @@
 #       format_name: percent
 #       format_version: '1.3'
 #       jupytext_version: 1.16.0
-#   kernelspec:
-#     display_name: calliope_dev
-#     language: python
-#     name: python3
 # ---
 
 # %% [markdown]
